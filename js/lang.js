ba9a065b0d4d2c515bcedf4f76099c567b74179f
HyFive Air <909761098@qq.com>
2018-03-29 00:47:47 +0800

    go chinese bar

---
+++ js/lang.js
@@ -0,0 +1,53 @@
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('.change-lang').outerHeight();
+	removeLanguage = false
+
+	$(window).scroll(function(event){
+	    didScroll = true;
+	});
+
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	    
+	    // Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    
+	    // If they scrolled down and are past the navbar, add class .nav-up.
+	    // This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight && removeLanguage == false){
+	        // Scroll Down
+	        $('.change-lang').removeClass('lang-show').addClass('lang-hide');
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height() && removeLanguage == false) {
+	            $('.change-lang').removeClass('lang-hide').addClass('lang-show')
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
+
+
+
+function removeLang(){
+		$('.change-lang').addClass('remove-lang');
+		removeLanguage = true;
+	}
+
+// function showLang(){
+// 	$('.change-lang').removeClass('lang-hide');
+// }
+// $(document).ready(function() {$('.change-lang').removeClass('lang-hide');});
+
+
