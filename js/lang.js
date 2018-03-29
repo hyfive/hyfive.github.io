fd84aa6ce785eeed62d870bca5c7f4b95b79ec3f
HyFive Air <909761098@qq.com>
2018-03-29 12:06:09 +0800

    chinese

--- js/lang.js
+++ js/lang.js
@@ -1,42 +1,42 @@
-	// Hide Header on on scroll down
-	var didScroll;
-	var lastScrollTop = 0;
-	var delta = 5;
-	var navbarHeight = $('.change-lang').outerHeight();
-	removeLanguage = false
+	// // Hide Header on on scroll down
+	// var didScroll;
+	// var lastScrollTop = 0;
+	// var delta = 5;
+	// var navbarHeight = $('.change-lang').outerHeight();
+	// removeLanguage = false
 
-	$(window).scroll(function(event){
-	    didScroll = true;
-	});
+	// $(window).scroll(function(event){
+	//     didScroll = true;
+	// });
 
-	setInterval(function() {
-	    if (didScroll) {
-	        hasScrolled();
-	        didScroll = false;
-	    }
-	}, 250);
+	// setInterval(function() {
+	//     if (didScroll) {
+	//         hasScrolled();
+	//         didScroll = false;
+	//     }
+	// }, 250);
 
-	function hasScrolled() {
-	    var st = $(this).scrollTop();
+	// function hasScrolled() {
+	//     var st = $(this).scrollTop();
 	    
-	    // Make sure they scroll more than delta
-	    if(Math.abs(lastScrollTop - st) <= delta)
-	        return;
+	//     // Make sure they scroll more than delta
+	//     if(Math.abs(lastScrollTop - st) <= delta)
+	//         return;
 	    
-	    // If they scrolled down and are past the navbar, add class .nav-up.
-	    // This is necessary so you never see what is "behind" the navbar.
-	    if (st > lastScrollTop && st > navbarHeight && removeLanguage == false){
-	        // Scroll Down
-	        $('.change-lang').removeClass('lang-show').addClass('lang-hide');
-	    } else {
-	        // Scroll Up
-	        if(st + $(window).height() < $(document).height() && removeLanguage == false) {
-	            $('.change-lang').removeClass('lang-hide').addClass('lang-show')
-	        }
-	    }
+	//     // If they scrolled down and are past the navbar, add class .nav-up.
+	//     // This is necessary so you never see what is "behind" the navbar.
+	//     if (st > lastScrollTop && st > navbarHeight && removeLanguage == false){
+	//         // Scroll Down
+	//         $('.change-lang').removeClass('lang-show').addClass('lang-hide');
+	//     } else {
+	//         // Scroll Up
+	//         if(st + $(window).height() < $(document).height() && removeLanguage == false) {
+	//             $('.change-lang').removeClass('lang-hide').addClass('lang-show')
+	//         }
+	//     }
 	    
-	    lastScrollTop = st;
-	}
+	//     lastScrollTop = st;
+	// }
 
 
 
